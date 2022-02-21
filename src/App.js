6c333980169c09b6ceacf4b603b4adf61233cc3e
gnanesh05 <gnanesh007.mk@gmail.com>
2022-02-21 21:49:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [Advice, setAdvice] = useState('')
+const [click, setClick] = useState(false)
+useEffect(()=>{
+ //console.log("Effect")
+ //setAdvice(false)
+ fetchAdvice()
+},[])
+
+const fetchAdvice = ()=>{
+    console.log('clicked')
+   axios.get("https://api.adviceslip.com/advice")
+        .then((res)=>{
+           setAdvice(res.data.slip.advice)
+           console.log(res.data.slip.advice)
+        })
+        .catch((error)=>{
+           console.error(error)
+        })
 }
 
-export default App;
+
+  return (
+    <div className='app'>
+        <div className='card'>
+            <h1 className='heading'>{Advice}</h1>
+            <button className='button' onClick={fetchAdvice}>
+                <span>Give me Advice</span>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default App
